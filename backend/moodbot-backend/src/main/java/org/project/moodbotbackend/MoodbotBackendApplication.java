5a8d1d35001294ca79af82420cea81510085f791
Isla <islarcherjr@icloud.com>
2025-06-21 15:58:24 +0100

    backend: initial commit.

---
+++ backend/moodbot-backend/src/main/java/org/project/moodbotbackend/MoodbotBackendApplication.java
@@ -0,0 +1,13 @@
+package org.project.moodbotbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodbotBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoodbotBackendApplication.class, args);
+    }
+
+}
---
+++ backend/moodbot-backend/src/test/java/org/project/moodbotbackend/MoodbotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.moodbotbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodbotBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

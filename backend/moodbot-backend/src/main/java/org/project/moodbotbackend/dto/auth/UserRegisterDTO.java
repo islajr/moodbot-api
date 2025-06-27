74fb0a9fb4a02fbca9cdc2018ff62d5399c07506
Isla <islarcherjr@icloud.com>
2025-06-27 22:51:29 +0100

    backend: update - slight change to registration input validation.

--- backend/moodbot-backend/src/main/java/org/project/moodbotbackend/dto/auth/UserRegisterDTO.java
+++ backend/moodbot-backend/src/main/java/org/project/moodbotbackend/dto/auth/UserRegisterDTO.java
@@ -10,18 +10,18 @@ public record UserRegisterDTO(
 
         @NotNull
         @NotBlank(message = "a username is required")
-        @Size(min = 1, max = 20)
+        @Size(min = 1, max = 20, message = "username can only be between 1 and 20 characters long")
         String username,
 
         @NotNull
         @NotBlank(message = "an e-mail address is required")
         @Email(message = "provide a valid e-mail address")
-        @Size(min = 5, max = 50)
+        @Size(min = 5, max = 50, message = "email must be between 5 and 50 characters long")
         String email,
 
         @NotNull
         @NotBlank(message = "a password is required")
-        @Size(min = 8, max = 75)
+        @Size(min = 8, max = 75, message = "password must be between 8 and 75 characters long")
         String password
 ) {
 

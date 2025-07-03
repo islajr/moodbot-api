d588540c4e378d8c773b0dd0ed139f7a157d9cb2
Isla <islarcherjr@icloud.com>
2025-07-03 19:46:01 +0100

    backend: update cors config.

--- backend/moodbot-backend/src/main/java/org/project/moodbotbackend/security/CustomCorsConfiguration.java
+++ backend/moodbot-backend/src/main/java/org/project/moodbotbackend/security/CustomCorsConfiguration.java
@@ -13,7 +13,7 @@ public class CustomCorsConfiguration implements CorsConfigurationSource {
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:8080", "http://127.0.0.1:8080"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
 
         return config;

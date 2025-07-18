0b0f902e13f2c6f204a7590e5992d5d502c2203f
Isla <islarcherjr@icloud.com>
2025-07-18 22:02:50 +0100

    update: slight edit to Chat entity

--- backend/moodbot-backend/src/main/java/org/project/moodbotbackend/entity/Chat.java
+++ backend/moodbot-backend/src/main/java/org/project/moodbotbackend/entity/Chat.java
@@ -1,10 +1,19 @@
 package org.project.moodbotbackend.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -25,7 +34,7 @@ public class Chat {
     @JoinColumn(name = "user_id")
     User user;
 
-    ArrayList<ChatMessage> messages = new ArrayList<>();
+    ArrayList<ChatMessage> messages;
     LocalDateTime createdAt;
     LocalDateTime updatedAt;
 

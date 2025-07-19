ad80eb606bd964496ee1251a6f08576f1191b81f
Isla <islarcherjr@icloud.com>
2025-07-19 19:46:03 +0100

    backend: fix - finally fixed error with login ui

--- backend/moodbot-backend/src/main/resources/static/js/script.js
+++ backend/moodbot-backend/src/main/resources/static/js/script.js
@@ -4,12 +4,27 @@ const signUp = document.getElementById('signup-submit-button');
 const emailConfirmation = document.getElementById('email-confirmation');
 const globalURL = "https://moodbot-api.onrender.com";
 
+/* functions */
+
+// collect OTP values from input fields
+
+function getOTP() {
+    const otpInputs = document.querySelectorAll('.otp-input');
+    let otp = '';
+    otpInputs.forEach(input => {
+    otp += input.value;
+    });
+
+    return otp;
+}
+
+/* main code */
 
 // signup form submission handler
 document.getElementById('sign-up-form').addEventListener('submit', (e) => {
     e.preventDefault(); // prevent default form submission
 })
-signUp.addEventListener('click', function(event) {
+signUp.addEventListener('submit', function(event) {
     event.preventDefault();
     const username = document.getElementById('signup-username').value;
     const email = document.getElementById('signup-e-mail').value;
@@ -46,12 +61,7 @@ signUp.addEventListener('click', function(event) {
                 otpSubmit.addEventListener('click', async function(otpEvent) {
                     otpEvent.preventDefault();
 
-                    // collect OTP values from input fields
-                    const otpInputs = document.querySelectorAll('.otp-input');
-                    let otp = '';
-                    otpInputs.forEach(input => {
-                        otp += input.value;
-                    });
+                    let otp = getOTP(); // get OTP from input fields
 
                     try {
                         const verifyResponse = await fetch(globalURL + '/api/v1/moodbot/auth/confirm', {
@@ -61,7 +71,7 @@ signUp.addEventListener('click', function(event) {
                             },
                             body: JSON.stringify({ 
                                 "email": otpData.email, 
-                                "otp": Number(otp)  // convert OTP to number
+                                "code": Number(otp)  // convert OTP to number
                             })
                         });
 
@@ -110,7 +120,7 @@ signUp.addEventListener('click', function(event) {
 document.getElementById('login-form').addEventListener('submit', (e) => {
     e.preventDefault(); // prevent default form submission
 });
-loginForm.addEventListener('click', function(event) {
+loginForm.addEventListener('submit', function(event) {
     event.preventDefault();
     const identifier = document.getElementById('login-identifier').value;
     const password = document.getElementById('login-password').value;
@@ -130,24 +140,106 @@ loginForm.addEventListener('click', function(event) {
 
             // dealing with possible errors
             switch (loginResponse.status) {
-                case 400:
+                case Number (400):
                     alert("Please fill in all fields correctly.");
                     break;
-                case 401:
-                    alert("Please verify your email before logging in.");
+                case Number (401):
                     // redirect to email confirmation
+                    async function redirectToEmailConfirmation() {
+                        try {
+                            const triggerVerification = await fetch(globalURL + '/api/v1/moodbot/auth/verify', {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                },
+                                body: JSON.stringify({ 
+                                    "email": identifier
+                                })
+                            })
+
+                            switch (triggerVerification.status) {
+                                case Number(201):
+                                    alert("Please check your email for the OTP to verify your account.");
+                                    signupForm.style.display = 'none';
+                                    loginForm.style.style.display = 'none';
+                                    emailConfirmation.style.display = 'flex';
+                                    const otpSubmit = document.getElementById('confirm-otp');
+                                    otpSubmit.addEventListener('click', async function(otpEvent) {
+                                        otpEvent.preventDefault();
+                                        let otp = getOTP(); // get OTP from input fields
+                                        try {
+                                            const verifyResponse = await fetch(globalURL + '/api/v1/moodbot/auth/confirm', {
+                                                method: 'POST',
+                                                headers: {
+                                                    'Content-Type': 'application/json'
+                                                },
+                                                body: JSON.stringify({ 
+                                                    "email": identifier, 
+                                                    "code": Number(otp)  // convert OTP to number
+                                                })
+                                            });
+                                            
+                                            // dealing with possible errors
+                                            if (verifyResponse.status === 400) {
+                                                if (verifyResponse.json().message === "incorrect code") {
+                                                    alert("Incorrect OTP. Please try again.");
+                                                    return;
+                                                } else {
+                                                    alert("An error occurred. Please try again later.");
+                                                    return;
+                                                }
+                                            }
+                                        
+                                            // if no errors...
+                                            const verifyData = await verifyResponse.json();
+                                            localStorage.setItem('accessToken', verifyData.accessToken);
+                                            localStorage.setItem('refreshToken', verifyData.refreshToken);
+                                            alert("OTP verified successfully!");
+                                            // redirect to chat page
+                                            window.location.href = '/chatpage';
+                                        } catch (error) {
+                                            console.error('Error:', error);
+                                            alert('An error occurred while verifying the OTP. Please try again.');
+                                        }
+                                    })
+
+                                    break;
+                                case Number(404):
+                                    alert("User not found. Please check your credentials.");
+                                    break
+                                case Number(500):
+                                    alert("There was an error, but don't worry, it's not yout fault. Please try again later.");
+                                    break;
+                                default:
+                                    alert("An error occurred while verifying your email. Please try again later.");
+                                    break;
+                        }
+                    }
+                        catch (error) {
+                            console.error('Error:', error);
+                            alert('An error occurred while sending the verification email. Please try again.');
+                        }
+                    }
+                    redirectToEmailConfirmation();
+
                     break;
-                case 404:
+                case Number(404):
                     alert("User not found. Please check your credentials.");
                     break;
-                case 500:
+                case Number(500):
                     alert("An error occured, but don't worry, we're on it! Please try again later.");
                     break;
                 default:
+                    // if no errors...
+                    const loginData = await loginResponse.json();
+                    alert("Login successful!");
+                    localStorage.setItem('accessToken', loginData.accessToken);
+                    localStorage.setItem('refreshToken', loginData.refreshToken);
+                    window.location.href = '../html/chatpage.html';
                     break;
             }        
             
-            // if no errors...
+            /* // if no errors...
             const loginData = await loginResponse.json();
             if (loginData.success) {
                 alert("Login successful!");
@@ -156,12 +248,13 @@ loginForm.addEventListener('click', function(event) {
                 window.location.href = '/chatpage';
             } else {
                 alert(loginData.message);
-            }
+            } */
         } catch (error) {
             console.error('Error:', error);
             alert('An error occurred while logging in. Please try again.');
         }
     }
+    getLogin();
 });
 
 /* accessibility logic */

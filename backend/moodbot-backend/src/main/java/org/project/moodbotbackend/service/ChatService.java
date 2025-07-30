6998ee2d09cd01d90a31973c132195b619310f9e
Isla <islarcherjr@icloud.com>
2025-07-31 00:38:04 +0100

    backend: untested - added rest template request to ai service.

--- backend/moodbot-backend/src/main/java/org/project/moodbotbackend/service/ChatService.java
+++ backend/moodbot-backend/src/main/java/org/project/moodbotbackend/service/ChatService.java
@@ -3,18 +3,24 @@ package org.project.moodbotbackend.service;
 import java.security.Principal;
 import java.time.LocalDateTime;
 
+import org.project.moodbotbackend.dto.app.AiRequestDTO;
+import org.project.moodbotbackend.dto.app.AiResponseDTO;
 import org.project.moodbotbackend.dto.app.ChatMessageDTO;
 import org.project.moodbotbackend.entity.Chat;
 import org.project.moodbotbackend.entity.ChatMessage;
 import org.project.moodbotbackend.entity.User;
 import org.project.moodbotbackend.entity.UserPrincipal;
+import org.project.moodbotbackend.exceptions.auth.AuthException;
 import org.project.moodbotbackend.repository.AuthRepository;
 import org.project.moodbotbackend.repository.ChatRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +30,10 @@ public class ChatService {
     private final ChatRepository chatRepository;
     private final AuthRepository authRepository;
     private final MyUserDetailsService myUserDetailsService;
+    private final RestTemplate restTemplate;
+
+    @Value("${moodbot.ai.service.url}")
+    private String aiServiceURL;
 
     public void saveMessage(ChatMessageDTO messageDTO, String sessionId, Principal user) {
         String identifier = SecurityContextHolder.getContext().getAuthentication().getName();
@@ -76,5 +86,55 @@ public class ChatService {
                message.getContent(),
                message.getTimestamp()
         );
+    }
+
+    public ChatMessageDTO generateResponse(ChatMessageDTO chatMessageDTO, Principal user) {
+
+        Chat chat = chatRepository.findChatBySessionId(chatMessageDTO.sessionId());
+
+        // preparing the request
+        User savedUser = authRepository.findUserByUsername(user.getName());
+
+        AiRequestDTO request = AiRequestDTO.builder()
+                .user_id(savedUser.getId().toString())
+                .message(chatMessageDTO.content())
+                .build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<AiRequestDTO> httpEntity = new HttpEntity<>(request, headers);
+
+        ResponseEntity<AiResponseDTO> response = restTemplate.exchange(
+                aiServiceURL,
+                HttpMethod.POST,
+                httpEntity,
+                AiResponseDTO.class
+
+        );
+
+        AiResponseDTO responseDTO = response.getBody();
+
+        // convert ai response to chat message for persisting
+        if (responseDTO != null) {
+
+            ChatMessage chatMessage = ChatMessage.builder()
+                    .content(responseDTO.response())
+                    .sender("BOT")
+                    .sessionId(chatMessageDTO.sessionId())
+                    .userId(null)
+                    .timestamp(LocalDateTime.now())
+                    .build();
+
+            chat.addMessage(chatMessage);
+            if (chat.getSlug().equals("default-slug"))
+                    chat.setSlug(responseDTO.slug());
+
+            chat.setUpdatedAt(LocalDateTime.now());
+            chatRepository.save(chat);
+            return toDto(chatMessage);
+        }
+        throw new AuthException(502, "Bad Gateway");
+
     }
 }

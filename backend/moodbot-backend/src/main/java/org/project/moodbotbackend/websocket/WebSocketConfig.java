5da0c1e7400e0379f614c5f5533585528e818607
Isla <islarcherjr@icloud.com>
2025-08-03 04:55:51 +0100

    backend: update - updates and fixes.

--- backend/moodbot-backend/src/main/java/org/project/moodbotbackend/websocket/WebSocketConfig.java
+++ backend/moodbot-backend/src/main/java/org/project/moodbotbackend/websocket/WebSocketConfig.java
@@ -1,28 +1,26 @@
 package org.project.moodbotbackend.websocket;
 
 import lombok.AllArgsConstructor;
-import org.springframework.context.annotation.Bean;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.security.messaging.context.SecurityContextChannelInterceptor;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
-
-import java.security.Principal;
-import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final JwtHandshakeInterceptor jwtHandshakeInterceptor;
     private final AuthChannelInterceptor authChannelInterceptor;
+
+    @Value("${moodbot.client.url}")
+    private String clientURL;
 
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
@@ -33,10 +31,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+
         registry
                 .addEndpoint("/chat")
                 .addInterceptors(jwtHandshakeInterceptor)
-                .setAllowedOriginPatterns("http://localhost:63342")
+                .setAllowedOriginPatterns(clientURL)
                 .withSockJS();
     }
 
